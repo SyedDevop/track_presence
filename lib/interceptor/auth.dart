26f18285fb84cd92053b2f1c4e85116ce1910772
SyedDevop <syeds.devops007@gmail.com>
2025-09-22 16:50:09 +0530

    refactor(auth): rewrite AuthInterceptor for improved token refresh and retry logic

--- lib/interceptor/auth.dart
+++ lib/interceptor/auth.dart
@@ -1,102 +1,213 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:vcare_attendance/api/error.dart';
+import 'package:vcare_attendance/interceptor/error.dart';
 import 'package:vcare_attendance/interceptor/logger.dart';
-import 'package:vcare_attendance/router/router_name.dart';
 import 'package:vcare_attendance/router/router.dart';
+import 'package:vcare_attendance/router/router_name.dart';
+import 'package:vcare_attendance/utils/jwtToken.dart';
 import 'package:vcare_attendance/utils/token_storage.dart';
 
 class AuthInterceptor extends QueuedInterceptor {
-  final Dio _dio;
-  final TokenStorage _storage;
-  AuthInterceptor(this._dio, this._storage);
+  AuthInterceptor(this._baseClient, this._storage) {
+    refreshClient = Dio();
+    refreshClient.options = BaseOptions(baseUrl: _baseClient.options.baseUrl);
+    refreshClient.interceptors.add(customErrorInterceptor);
+    if (kDebugMode) refreshClient.interceptors.add(dioLoggerInterceptor);
 
-  /// isRefreshing flag is used to control whether a token refresh is already in progress.
-  ///
-  /// If it's in progress, subsequent requests will skip the refresh attempt
-  /// until the first one completes.
-  bool isRefreshing = false;
-  final List<RequestOptions> _pendingRequests = [];
+    retryClient = Dio();
+    retryClient.options = BaseOptions(baseUrl: _baseClient.options.baseUrl);
+    retryClient.interceptors.add(customErrorInterceptor);
+    if (kDebugMode) retryClient.interceptors.add(dioLoggerInterceptor);
+  }
+
+  /// The storage to load and save the JWT token.
+  final TokenStorage _storage;
+
+  /// The base client to make requests.
+  final Dio _baseClient;
+
+  /// The client to make requests to refresh the JWT token.
+  late final Dio refreshClient;
+
+  /// The client to retry the request after refreshing the JWT token.
+  late final Dio retryClient;
 
   @override
   void onRequest(
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    if (options.path.contains('login.php') ||
-        options.path.contains('refresh.php')) {
+    try {
+      if (options.path.contains('login.php') ||
+          options.path.contains('refresh.php')) {
+        return handler.next(options);
+      }
+      String? token = await _storage.accessToken;
+      if (token == null) {
+        return handler.reject(
+          RevokeTokenException(requestOptions: options),
+          true,
+        );
+      }
+
+      final jwt = JwtToken.fromRawToken(token);
+
+      if (jwt.isValid()) {
+        options.headers.addAll(_buildHeaders(token));
+        return handler.next(options);
+      } else {
+        token = await _refreshToken(options: options);
+        options.headers.addAll(_buildHeaders(token));
+        return handler.next(options);
+      }
+    } on DioException catch (error) {
+      if (error.response != null && error.response!.statusCode == 401) {
+        return handler.reject(
+          RevokeTokenException(requestOptions: options),
+          true,
+        );
+      }
+      return handler.reject(error);
+    } on Exception {
       return handler.next(options);
     }
-    //TODO: check if the request is a login or refresh Token then don't auth headers
-    final token = await _storage.accessToken;
-    if (token != null) {
-      options.headers.addAll({"Authorization": "Bearer $token"});
-    }
-    return handler.next(options);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    if (err.response?.statusCode != 401) return handler.next(err);
-
-    // Token is refreshing Token the return.
-    if (isRefreshing) return handler.next(err);
-    isRefreshing = true;
-    final errorType = err.response?.data['error'] as String? ?? "";
-    // print("Start Dio error Auth");
-    if (errorType == kTokenExpired) {
-      // print("Try Refresh =  Type: $errorType, retried: $retried");
-      try {
-        final accessToken = await _refreshToken();
-        if (accessToken == null) {
-          await _storage.clear();
-          router.pushReplacementNamed(RouteNames.login);
-          return handler.reject(err);
-        } else {
-          // Retry the original request.
-          final opts = err.requestOptions;
-          opts.headers['Authorization'] = 'Bearer $accessToken';
-          final clone = await _dio.fetch(opts);
-          return handler.resolve(clone);
-        }
-      } catch (e) {
-        // refresh failed
-        if (kDebugMode) print('[Error] Refresh In On Error Token: $e');
-        await _storage.clear();
-        router.pushReplacementNamed(RouteNames.login);
-      }
-    } else if (errorType == kTokenNotFound || errorType == kTokenInvalid) {
-      // print("Try valid or not found =  Type: $errorType, retried: $retried");
-      // Access token invalid or not found
+    if (err is RevokeTokenException) {
+      final errorType = err.response?.data['error'] as String? ?? "";
       await _storage.clear();
-      router.pushReplacementNamed(RouteNames.login);
-      return;
-    }
-    return handler.next(err);
-  }
-
-  Future<String?> _refreshToken() async {
-    try {
-      final dioRefresh = Dio(_dio.options);
-      final refreshToken = await _storage.refreshToken;
-      if (refreshToken == null) return null;
-
-      if (kDebugMode) dioRefresh.interceptors.add(dioLoggerInterceptor);
-      final response = await dioRefresh
-          .post('refresh.php', data: {'refresh_token': refreshToken});
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final newAccess = response.data['access_token'] as String;
-        final newRefresh = response.data['refresh_token'] as String;
-        await _storage.save(access: newAccess, refresh: newRefresh);
-        return newAccess;
-      } else if (response.statusCode == 401) {
-        return null;
-      } else {
-        return null;
+      if (errorType == kTokenNotFound || errorType == kTokenInvalid) {
+        router.pushReplacementNamed(RouteNames.login);
+        return;
       }
-    } catch (e) {
-      if (kDebugMode) print('[Error] Refresh Token: $e');
-      return null;
+      return handler.reject(err);
+    }
+
+    /// If the response status code is not 401, continue with the error.
+    if (!shouldRefresh(err.response)) {
+      return handler.next(err);
+    }
+
+    String? token = await _storage.accessToken;
+
+    /// If the JWT token is null, reject the request.
+    if (token == null) {
+      return handler.reject(err);
+    }
+
+    final jwt = JwtToken.fromRawToken(token);
+
+    try {
+      if (jwt.isValid()) {
+        final previousRequest = await retry(
+          retryClient: retryClient,
+          requestOptions: err.requestOptions,
+          buildHeaders: _buildHeaders(token),
+        );
+
+        return handler.resolve(previousRequest);
+      } else {
+        token = await _refreshToken(options: err.requestOptions);
+
+        /// Retry the request.
+        final previousRequest = await retry(
+          retryClient: retryClient,
+          requestOptions: err.requestOptions,
+          buildHeaders: _buildHeaders(token),
+        );
+
+        return handler.resolve(previousRequest);
+      }
+    } on RevokeTokenException {
+      final errorType = err.response?.data['error'] as String? ?? "";
+      await _storage.clear();
+      if (errorType == kTokenNotFound || errorType == kTokenInvalid) {
+        router.pushReplacementNamed(RouteNames.login);
+        return;
+      }
+      return handler.reject(err);
+    } on DioException catch (err) {
+      return handler.next(err);
     }
   }
+
+  Future<Response> retry({
+    required Dio retryClient,
+    required RequestOptions requestOptions,
+    required Map<String, String> buildHeaders,
+  }) async {
+    return retryClient.request(
+      requestOptions.path,
+      cancelToken: requestOptions.cancelToken,
+      data: requestOptions.data is FormData
+          ? (requestOptions.data as FormData).clone()
+          : requestOptions.data,
+      onReceiveProgress: requestOptions.onReceiveProgress,
+      onSendProgress: requestOptions.onSendProgress,
+      queryParameters: requestOptions.queryParameters,
+      options: Options(
+        method: requestOptions.method,
+        sendTimeout: requestOptions.sendTimeout,
+        receiveTimeout: requestOptions.receiveTimeout,
+        extra: requestOptions.extra,
+        headers: requestOptions.headers..addAll(buildHeaders),
+        responseType: requestOptions.responseType,
+        contentType: requestOptions.contentType,
+        validateStatus: requestOptions.validateStatus,
+        receiveDataWhenStatusError: requestOptions.receiveDataWhenStatusError,
+        followRedirects: requestOptions.followRedirects,
+        maxRedirects: requestOptions.maxRedirects,
+        requestEncoder: requestOptions.requestEncoder,
+        responseDecoder: requestOptions.responseDecoder,
+        listFormat: requestOptions.listFormat,
+      ),
+    );
+  }
+
+  Future<String> _refreshToken({
+    required RequestOptions options,
+  }) async {
+    try {
+      final refreshToken = await _storage.refreshToken;
+      if (refreshToken == null) {
+        throw RevokeTokenException(requestOptions: options);
+      }
+      final response = await refreshClient.post(
+        'refresh.php',
+        data: {'refresh_token': refreshToken},
+      );
+
+      final newAccess = response.data['access_token'] as String;
+      final newRefresh = response.data['refresh_token'] as String;
+      await _storage.save(access: newAccess, refresh: newRefresh);
+      return newAccess;
+    } on DioException catch (e) {
+      if (kDebugMode) print('[Error] Refresh Token: $e');
+      if (e.response != null && e.response!.statusCode == 401) {
+        await _storage.clear();
+        throw RevokeTokenException(requestOptions: options);
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /// Builds the headers with the JWT token.
+  Map<String, String> _buildHeaders(String token) {
+    return {'Authorization': 'Bearer $token'};
+  }
+
+  /// Checks if the response should be refreshed.
+  bool shouldRefresh<R>(Response<R>? response) => response?.statusCode == 401;
+}
+
+/// {template dio_exception}
+/// Exception thrown when the token is revoked.
+/// {endtemplate}
+class RevokeTokenException extends DioException {
+  /// {template dio_exception}
+  RevokeTokenException({required super.requestOptions});
 }
